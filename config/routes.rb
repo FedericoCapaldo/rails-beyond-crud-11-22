68bc4462ecd3aedf09c24086661a0d9eba680d0d
Federico <federico.capaldo@hotmail.com>
2022-11-15 15:52:12 +0000

    added delete action for reviews, not a nested route

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
     resources :reviews, only: [:new, :create]
   end
 
+  resources :reviews, only: [:destroy]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
